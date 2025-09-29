0098406c7b9a20b01cf7fa40bb99449d26628558
C2JanDom <c2jandom@vasvari.org>
2025-09-29 09:17:26 +0200

    masodik feladat kesz

---
+++ masodik.py
@@ -0,0 +1,10 @@
+print("Adja meg a téglalap egyik hosszát (cm): ")
+hossz1 = float(input())
+
+print("Adja meg a téglalap másik hosszát (cm): ")
+hossz2 = float(input())
+
+terulet = hossz1 * hossz2
+kerulet = 2*(hossz1+hossz2)
+
+print(f"A téglalap területe: {terulet} cm2, a téglalap kerülete: {kerulet} cm.")
